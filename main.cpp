585f41429953a5a4d55603242b584c4a0e276b53
James <jjcastle@utexas.edu>
2022-05-26 22:14:59 -0500

    Builtg out default constructor, parameterized constructor, and copy constructor.

--- main.cpp
+++ main.cpp
@@ -2,26 +2,44 @@
 
 using namespace std;
 //Implement the class Box
-//l,b,h are integers representing the dimensions of the box
+class Box {
+    private:
+    //l,b,h are integers representing the dimensions of the box
+    int l = 0;
+    int b = 0;
+    int h = 0;
 
-// The class should have the following functions :
+    public:
+    // The class should have the following functions :
+    // Constructors:
+    Box () {
+        l = 0;
+        b = 0;
+        h = 0;
+    }
+    // Box(int,int,int);
+    Box (int length, int breadth, int height) {
+        l = length;
+        b = breadth;
+        h = height;
+    }
+    // Box(Box);
+    Box (const Box&) {
 
-// Constructors:
-// Box();
-// Box(int,int,int);
-// Box(Box);
+    }
 
 
-// int getLength(); // Return box's length
-// int getBreadth (); // Return box's breadth
-// int getHeight ();  //Return box's height
-// long long CalculateVolume(); // Return the volume of the box
+    // int getLength(); // Return box's length
+    // int getBreadth (); // Return box's breadth
+    // int getHeight ();  //Return box's height
+    // long long CalculateVolume(); // Return the volume of the box
 
-//Overload operator < as specified
-//bool operator<(Box& b)
+    //Overload operator < as specified
+    //bool operator<(Box& b)
 
-//Overload operator << as specified
-//ostream& operator<<(ostream& out, Box& B)
+    //Overload operator << as specified
+    //ostream& operator<<(ostream& out, Box& B)
+    };
 
 
 void check2()
